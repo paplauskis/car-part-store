6cc3e617fb7d312e418a30340175f25bd9928415
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-01 16:36:49 +0300

    initial commit

---
+++ src/main/java/org/carpartstore/CarPartStoreApplication.java
@@ -0,0 +1,13 @@
+package org.carpartstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPartStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarPartStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/carpartstore/CarPartStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carpartstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPartStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
