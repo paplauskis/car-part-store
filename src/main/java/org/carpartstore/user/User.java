324a5f336130c1d87ac96ba8b254da020ecac87f
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-10-06 18:35:09 +0300

    created user entity

---
+++ src/main/java/org/carpartstore/user/User.java
@@ -0,0 +1,34 @@
+package org.carpartstore.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "\"user\"")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
